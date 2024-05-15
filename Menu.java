2d78c0415215c5659da11e060350ae986bee69ef
Jhosseph <jhossephgonzalez@gmail.com>
2024-05-15 20:38:23 -0300

    Add menu class

---
+++ Menu.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Menu {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        CarManager carManager = new CarManager();
+
+        System.out.println("Informe o ano do veiculo: ");
+        int year = scan.nextInt();
+        scan.nextLine();
+
+        System.out.println("Informe o valor de venda do veiculo: ");
+        int saleValue = scan.nextInt();
+        scan.nextLine();
+
+        System.out.println("Informe o valor pelo qual o veiculo foi comprado: ");
+        int purchaseValue = scan.nextInt();
+        scan.nextLine();
+
+        System.out.println("Informe o modelo do carro: ");
+        String model = scan.nextLine();
+
+        System.out.println("Informe a marca do carro: ");
+        String brand = scan.nextLine();
+
+        CarDTO carDTO = new CarDTO(year, saleValue, purchaseValue, model, brand);
+
+        carManager.addCar(carDTO);
+
+    }
+}

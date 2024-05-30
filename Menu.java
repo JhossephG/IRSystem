08ab856e30eb76eb07419cff5c2ff218d675a8a9
Jhosseph <jhossephgonzalez@gmail.com>
2024-05-30 15:19:58 -0300

    Update

--- Menu.java
+++ Menu.java
@@ -1,30 +1,32 @@
 import java.util.Scanner;
 public class Menu {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        CarManager carManager = new CarManager();
+        
+        
+        // Scanner scan = new Scanner(System.in);
+        // CarManager carManager = new CarManager();
 
-        System.out.println("Informe o ano do veiculo: ");
-        int year = scan.nextInt();
-        scan.nextLine();
+        // System.out.println("Informe o ano do veiculo: ");
+        // int year = scan.nextInt();
+        // scan.nextLine();
 
-        System.out.println("Informe o valor de venda do veiculo: ");
-        int saleValue = scan.nextInt();
-        scan.nextLine();
+        // System.out.println("Informe o valor de venda do veiculo: ");
+        // int saleValue = scan.nextInt();
+        // scan.nextLine();
 
-        System.out.println("Informe o valor pelo qual o veiculo foi comprado: ");
-        int purchaseValue = scan.nextInt();
-        scan.nextLine();
+        // System.out.println("Informe o valor pelo qual o veiculo foi comprado: ");
+        // int purchaseValue = scan.nextInt();
+        // scan.nextLine();
 
-        System.out.println("Informe o modelo do carro: ");
-        String model = scan.nextLine();
+        // System.out.println("Informe o modelo do carro: ");
+        // String model = scan.nextLine();
 
-        System.out.println("Informe a marca do carro: ");
-        String brand = scan.nextLine();
+        // System.out.println("Informe a marca do carro: ");
+        // String brand = scan.nextLine();
 
-        CarDTO carDTO = new CarDTO(year, saleValue, purchaseValue, model, brand);
+        // CarDTO carDTO = new CarDTO(year, saleValue, purchaseValue, model, brand);
 
-        carManager.addCar(carDTO);
+        // carManager.addCar(carDTO);
 
     }
 }

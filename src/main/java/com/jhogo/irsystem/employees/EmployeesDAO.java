646fd9652ba888fdeb720ce163c6198e138d56e9
Jhosseph <jhossephgonzalez@gmail.com>
2024-05-31 16:45:19 -0300

    Add methods for Service and DAO Employee classes

--- src/main/java/com/jhogo/irsystem/employees/EmployeesDAO.java
+++ src/main/java/com/jhogo/irsystem/employees/EmployeesDAO.java
@@ -19,6 +19,7 @@ public class EmployeesDAO {
             statement.setString(4, employees.getUserName());
             statement.setString(5, employees.getPassword());
             statement.setDate(6, employees.getBirthday());
+            statement.executeUpdate();
         }
     }
     
@@ -32,7 +33,7 @@ public class EmployeesDAO {
                     employee.setId(resultSet.getInt("id"));
                     employee.setName(resultSet.getString("name"));
                     employee.setRole(resultSet.getString("role"));
-                    employee.setAddress(resultSet.getString("addres"));
+                    employee.setAddress(resultSet.getString("address"));
                     employee.setUserName(resultSet.getString("username"));
                     employee.setPassword(resultSet.getString("password"));
                     employee.setBirthday(resultSet.getDate("birthday"));
@@ -40,6 +41,27 @@ public class EmployeesDAO {
                 }
             }
             return employees;
+    }
+
+    public void updateEmployee (Employees employees) throws SQLException {
+        String sql = "UPDATE Employee SET name=?, role=?, address=?, username=?, password=?, birthday=? WHERE id=?";
+        try(PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, employees.getName());
+            statement.setString(2, employees.getRole());
+            statement.setString(3, employees.getAddress());
+            statement.setString(4, employees.getUserName());
+            statement.setString(5, employees.getPassword());
+            statement.setDate(6, employees.getBirthday());
+            statement.executeUpdate();
+        }
+    }
+
+    public void deleteEmployee (int employeeId) throws SQLException {
+        String sql = "DELETE FROM Employee WHERE id=?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)){
+            statement.setInt(1, employeeId);
+            statement.executeUpdate();
+        }
     }
     
 }
--- src/main/java/com/jhogo/irsystem/employees/EmployeesService.java
+++ src/main/java/com/jhogo/irsystem/employees/EmployeesService.java
@@ -1,5 +1,7 @@
 package src.main.java.com.jhogo.irsystem.employees;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class EmployeesService {
     private EmployeesDAO employeesDAO;
@@ -17,5 +19,37 @@ public class EmployeesService {
         employees.setRole(employeesDTO.getRole());
         employees.setUserName(employeesDTO.getUserName());
         employeesDAO.insertEmployee(employees);
+    }
+
+    private List<EmployeesDTO> getAllEmployees() throws SQLException {
+        List<Employees> employees = employeesDAO.getAllEmployees();
+        return employees.stream().map(employee -> {
+            EmployeesDTO employeesDTO = new EmployeesDTO();
+            employeesDTO.setId(employee.getId());
+            employeesDTO.setName(employee.getName());
+            employeesDTO.setAddress(employee.getAddress());
+            employeesDTO.setBirthday(employee.getBirthday());
+            employeesDTO.setPassword(employee.getPassword());
+            employeesDTO.setRole(employee.getRole());
+            employeesDTO.setUserName(employee.getUserName());
+            return employeesDTO;
+        }).collect(Collectors.toList());
+    }
+
+    private void updateEmployee (EmployeesDTO employeesDTO, int employeeId) throws SQLException {
+        Employees employees = new Employees();
+        employees.setId(employeesDTO.getId());
+        employees.setName(employeesDTO.getName());
+        employees.setAddress(employeesDTO.getAddress());
+        employees.setBirthday(employeesDTO.getBirthday());
+        employees.setPassword(employeesDTO.getPassword());
+        employees.setRole(employeesDTO.getRole());
+        employees.setUserName(employeesDTO.getUserName());
+        employeesDAO.updateEmployee(employees);
+
+    }
+
+    private void removeEmployee(int employeeId) throws SQLException {
+        employeesDAO.deleteEmployee(employeeId);
     }
 }

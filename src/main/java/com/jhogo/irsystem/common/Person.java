dc75deba067cec16c3be4df479dc6d4ab12202dc
Jhosseph <jhossephgonzalez@gmail.com>
2024-05-31 20:50:43 -0300

    Create abstract class Person to be used in User and Employee

---
+++ src/main/java/com/jhogo/irsystem/common/Person.java
@@ -0,0 +1,35 @@
+package src.main.java.com.jhogo.irsystem.common;
+import java.sql.Date;
+
+public abstract class Person {
+    private int id;
+    private String name;
+    private String address;
+    private Date birthday;
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public Date getBirthday() {
+        return birthday;
+    }
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}

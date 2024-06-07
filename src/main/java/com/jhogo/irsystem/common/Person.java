06564ccf3ed29b7151363ca990476f417607cd72
Jhosseph <jhossephgonzalez@gmail.com>
2024-06-06 21:38:39 -0300

    Add more fields that make sense to belong to Person entity

--- src/main/java/com/jhogo/irsystem/common/Person.java
+++ src/main/java/com/jhogo/irsystem/common/Person.java
@@ -3,10 +3,14 @@ import java.sql.Date;
 
 public abstract class Person {
     private int id;
-    private String name;
+    private String idNumber;
+    private String phoneNumber;
+    private String email;
+    private String fullName;
+    private String username;
     private String password;
     private String address;
-    private Date birthday;
+    private Date birthDate;
 
     // Getters and setters
     public int getId() {
@@ -15,11 +19,11 @@ public abstract class Person {
     public void setId(int id) {
         this.id = id;
     }
-    public String getName() {
-        return name;
+    public String getFullName() {
+        return fullName;
     }
-    public void setName(String name) {
-        this.name = name;
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
     }
     public String getAddress() {
         return address;
@@ -27,16 +31,40 @@ public abstract class Person {
     public void setAddress(String address) {
         this.address = address;
     }
-    public Date getBirthday() {
-        return birthday;
+    public Date getBirthDate() {
+        return birthDate;
     }
-    public void setBirthday(Date birthday) {
-        this.birthday = birthday;
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
     }
     public String getPassword() {
         return password;
     }
     public void setPassword(String password) {
         this.password = password;
+    }
+    public String getIdNumber() {
+        return idNumber;
+    }
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String userName) {
+        this.username = userName;
     }
 }

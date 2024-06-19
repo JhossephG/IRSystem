19428bd19ed875dc82951ef3b924d05648f33817
Jhosseph <jhossephgonzalez@gmail.com>
2024-06-19 16:37:13 -0300

    Handling exceptions in Vehicle service layer

--- src/main/java/com/jhogo/irsystem/service/VehicleService.java
+++ src/main/java/com/jhogo/irsystem/service/VehicleService.java
@@ -1,5 +1,6 @@
 package com.jhogo.irsystem.service;
 import com.jhogo.irsystem.dto.VehicleDTO;
+import com.jhogo.irsystem.exception.CustomSQLException;
 import com.jhogo.irsystem.model.Vehicle;
 import com.jhogo.irsystem.repository.VehicleDAO;
 
@@ -10,55 +11,68 @@ import java.util.stream.Collectors;
 public class VehicleService {
     private final VehicleDAO vehicleDAO;
 
-    public VehicleService(VehicleDAO vehicleDAO) {
+    public VehicleService (VehicleDAO vehicleDAO) {
         this.vehicleDAO = vehicleDAO;
     }
 
-    public void addCar(VehicleDTO vehicleDTO) throws SQLException {
-        Vehicle car = new Vehicle();
-        car.setModel(vehicleDTO.getModel());
-        car.setBrand(vehicleDTO.getBrand());
-        car.setVehicleIdNumber(vehicleDTO.getVehicleIdNumber());
-        car.setManufactureYear(vehicleDTO.getManufactureYear());
-        car.setSaleValue(vehicleDTO.getSaleValue());
-        car.setPurchaseValue(vehicleDTO.getPurchaseValue());
-        car.setAvailability(vehicleDTO.isAvailability());
-        vehicleDAO.insertVehicle(car);
+    public void addVehicle (VehicleDTO vehicleDTO) {
+        try {
+            Vehicle car = new Vehicle();
+            car.setModel(vehicleDTO.getModel());
+            car.setBrand(vehicleDTO.getBrand());
+            car.setVehicleIdNumber(vehicleDTO.getVehicleIdNumber());
+            car.setManufactureYear(vehicleDTO.getManufactureYear());
+            car.setSaleValue(vehicleDTO.getSaleValue());
+            car.setPurchaseValue(vehicleDTO.getPurchaseValue());
+            car.setAvailability(vehicleDTO.isAvailability());
+            vehicleDAO.insertVehicle(car);
+        } catch (SQLException e) {
+            throw new CustomSQLException("Error adding new vehicle", e);
+        }
     }
 
-    public List<VehicleDTO> getAllCars() throws SQLException {
-        List<Vehicle> cars = vehicleDAO.getAllVehicles();
-        return cars.stream().map(car -> {
+    public List<VehicleDTO> getAllVehicles () {
+        try {
+            List<Vehicle> cars = vehicleDAO.getAllVehicles();
+            return cars.stream().map(car -> {
+                VehicleDTO vehicleDTO = new VehicleDTO();
+                vehicleDTO.setId(car.getId());
+                vehicleDTO.setModel(car.getModel());
+                vehicleDTO.setBrand(car.getBrand());
+                vehicleDTO.setVehicleIdNumber(car.getVehicleIdNumber());
+                vehicleDTO.setManufactureYear(car.getManufactureYear());
+                vehicleDTO.setSaleValue(car.getSaleValue());
+                vehicleDTO.setPurchaseValue(car.getPurchaseValue());
+                vehicleDTO.setAvailability(car.isAvailability());
+                return vehicleDTO;
+            }).collect(Collectors.toList());
+        } catch (SQLException e) {
+            throw new CustomSQLException("Error getting vehicles list", e);
+        }
+    }
+
+    public VehicleDTO getVehicleById (int vehicleId) {
+        try {
+            Vehicle vehicle = vehicleDAO.getVehicleById(vehicleId);
             VehicleDTO vehicleDTO = new VehicleDTO();
-            vehicleDTO.setId(car.getId());
-            vehicleDTO.setModel(car.getModel());
-            vehicleDTO.setBrand(car.getBrand());
-            vehicleDTO.setVehicleIdNumber(car.getVehicleIdNumber());
-            vehicleDTO.setManufactureYear(car.getManufactureYear());
-            vehicleDTO.setSaleValue(car.getSaleValue());
-            vehicleDTO.setPurchaseValue(car.getPurchaseValue());
-            vehicleDTO.setAvailability(car.isAvailability());
+            vehicleDTO.setId(vehicle.getId());
+            vehicleDTO.setModel(vehicle.getModel());
+            vehicleDTO.setBrand(vehicle.getBrand());
+            vehicleDTO.setVehicleIdNumber(vehicle.getVehicleIdNumber());
+            vehicleDTO.setManufactureYear(vehicle.getManufactureYear());
+            vehicleDTO.setSaleValue(vehicle.getSaleValue());
+            vehicleDTO.setPurchaseValue(vehicle.getPurchaseValue());
+            vehicleDTO.setAvailability(vehicle.isAvailability());
             return vehicleDTO;
-        }).collect(Collectors.toList());
+        } catch (SQLException e) {
+            throw new CustomSQLException("Error getting vehicle with id "+vehicleId, e);
+        }
     }
 
-    public VehicleDTO getVehicleById (int vehicleId) throws SQLException {
-        Vehicle vehicle = vehicleDAO.getVehicleById(vehicleId);
-        VehicleDTO vehicleDTO = new VehicleDTO();
-        vehicleDTO.setId(vehicle.getId());
-        vehicleDTO.setModel(vehicle.getModel());
-        vehicleDTO.setBrand(vehicle.getBrand());
-        vehicleDTO.setVehicleIdNumber(vehicle.getVehicleIdNumber());
-        vehicleDTO.setManufactureYear(vehicle.getManufactureYear());
-        vehicleDTO.setSaleValue(vehicle.getSaleValue());
-        vehicleDTO.setPurchaseValue(vehicle.getPurchaseValue());
-        vehicleDTO.setAvailability(vehicle.isAvailability());
-        return vehicleDTO;
-    }
-
-    public void updateCar(VehicleDTO vehicleDTO, int carId) throws SQLException {
+    public void updateVehicle (VehicleDTO vehicleDTO, int vehicleId) {
+        try {
         Vehicle car = new Vehicle();
-        car.setId(carId);
+        car.setId(vehicleId);
         car.setModel(vehicleDTO.getModel());
         car.setBrand(vehicleDTO.getBrand());
         car.setVehicleIdNumber(vehicleDTO.getVehicleIdNumber());
@@ -67,9 +81,16 @@ public class VehicleService {
         car.setPurchaseValue(vehicleDTO.getPurchaseValue());
         car.setAvailability(vehicleDTO.isAvailability());
         vehicleDAO.updateVehicle(car);
+        } catch (SQLException e) {
+            throw new CustomSQLException("Error updating vehicle with id "+vehicleId, e);
+        }
     }
 
-    public void deleteVehicle(int vehicleId) throws SQLException {
-        vehicleDAO.deleteVehicle(vehicleId);
+    public void deleteVehicle(int vehicleId) {
+        try {
+            vehicleDAO.deleteVehicle(vehicleId);
+        } catch (SQLException e) {
+            throw new CustomSQLException("Error deleting vehicle with id "+vehicleId, e);
+        }
     }
 }

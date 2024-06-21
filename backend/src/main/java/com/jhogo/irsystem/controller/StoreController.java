87708352a40b710d4f2c70b82be3da73563a8377
Jhosseph <jhossephgonzalez@gmail.com>
2024-06-21 19:27:30 -0300

    Use singular names in endpoints

--- backend/src/main/java/com/jhogo/irsystem/controller/StoreController.java
+++ backend/src/main/java/com/jhogo/irsystem/controller/StoreController.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/stores")
+@RequestMapping("/store")
 public class StoreController {
 
     @Autowired
--- backend/src/main/java/com/jhogo/irsystem/controller/VehicleController.java
+++ backend/src/main/java/com/jhogo/irsystem/controller/VehicleController.java
@@ -11,7 +11,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/vehicles")
+@RequestMapping("/vehicle")
 public class VehicleController {
 
     @Autowired

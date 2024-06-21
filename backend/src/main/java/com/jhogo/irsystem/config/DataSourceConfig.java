9419f9080057df846e8a8badb5cbd258d7406f44
Jhosseph <jhossephgonzalez@gmail.com>
2024-06-21 15:22:45 -0300

    Set databases profiles

--- backend/src/main/java/com/jhogo/irsystem/config/DataSourceConfig.java
+++ backend/src/main/java/com/jhogo/irsystem/config/DataSourceConfig.java
@@ -1,7 +1,9 @@
 package com.jhogo.irsystem.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
@@ -9,13 +11,33 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
+    @Value("${DATABASE_URL}")
+    private String databaseUrl;
+
+    @Value("${DATABASE_USERNAME}")
+    private String databaseUsername;
+
+    @Value("${DATABASE_PASSWORD}")
+    private String databasePassword;
 
     @Bean
-    public DataSource dataSource() {
+    @Profile("dev")
+    public DataSource devDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl(databaseUrl);
+        dataSource.setUsername(databaseUsername);
+        dataSource.setPassword(databasePassword);
+        return dataSource;
+    }
+
+    @Bean
+    @Profile("test")
+    public DataSource testDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.h2.Driver");
         dataSource.setUrl("jdbc:h2:mem:testdb");
-        dataSource.setUsername("");
+        dataSource.setUsername("sa");
         dataSource.setPassword("");
         return dataSource;
     }

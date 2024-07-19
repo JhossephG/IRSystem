bac484218dd4610ba9ec469b29ecb5557d1492ac
Jhosseph <jhossephgonzalez@gmail.com>
2024-07-18 21:03:46 -0300

    Adapt to the new structure

--- backend/src/main/java/com/jhogo/irsystem/model/Vehicle.java
+++ backend/src/main/java/com/jhogo/irsystem/model/Vehicle.java
@@ -1,36 +1,19 @@
 package com.jhogo.irsystem.model;
+import com.jhogo.irsystem.common.Product;
+
 import java.math.BigDecimal;
 
-public class Vehicle {
-
-    private int id;
+public class Vehicle extends Product {
     private int manufactureYear;
-    private int user_id;
-    private int store_id;
-    private BigDecimal saleValue;
-    private BigDecimal purchaseValue;
     private String model;
     private String brand;
     private String vehicleIdNumber;
-    private boolean availability;
 
     public int getManufactureYear() {
         return manufactureYear;
     }
     public void setManufactureYear(int manufactureYear) {
         this.manufactureYear = manufactureYear;
-    }
-    public BigDecimal getSaleValue() {
-        return saleValue;
-    }
-    public void setSaleValue(BigDecimal saleValue) {
-        this.saleValue = saleValue;
-    }
-    public BigDecimal getPurchaseValue() {
-        return purchaseValue;
-    }
-    public void setPurchaseValue(BigDecimal purchaseValue) {
-        this.purchaseValue = purchaseValue;
     }
     public String getModel() {
         return model;
@@ -49,30 +32,6 @@ public class Vehicle {
     }
     public void setVehicleIdNumber(String vehicleIdNumber) {
         this.vehicleIdNumber = vehicleIdNumber;
-    }
-    public boolean isAvailability() {
-        return availability;
-    }
-    public void setAvailability(boolean availability) {
-        this.availability = availability;
-    }
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public int getUser_id() {
-        return user_id;
-    }
-    public void setUser_id(int user_id) {
-        this.user_id = user_id;
-    }
-    public int getStore_id() {
-        return store_id;
-    }
-    public void setStore_id(int store_id) {
-        this.store_id = store_id;
     }
 }
     

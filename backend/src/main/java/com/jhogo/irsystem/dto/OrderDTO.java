151c006b8be60082770d888efceb1d5cf2e082f1
Jhosseph <jhossephgonzalez@gmail.com>
2024-07-21 23:00:34 -0300

    Add new DTO classes

---
+++ backend/src/main/java/com/jhogo/irsystem/dto/OrderDTO.java
@@ -0,0 +1,32 @@
+package com.jhogo.irsystem.dto;
+import java.security.Timestamp;
+
+public class OrderDTO {
+    private Timestamp order_date;
+    private double total_amount;
+    private String status;
+
+    public Timestamp getOrder_date() {
+        return order_date;
+    }
+
+    public void setOrder_date(Timestamp order_date) {
+        this.order_date = order_date;
+    }
+
+    public double getTotal_amount() {
+        return total_amount;
+    }
+
+    public void setTotal_amount(double total_amount) {
+        this.total_amount = total_amount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ backend/src/main/java/com/jhogo/irsystem/dto/OrderItemsDTO.java
@@ -0,0 +1,31 @@
+package com.jhogo.irsystem.dto;
+
+public class OrderItemsDTO {
+    private int quantity;
+    private Double price;
+    private String product_name;
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+}
